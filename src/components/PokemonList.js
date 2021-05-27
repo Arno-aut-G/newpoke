59eb478b140c9b34169fe5805a6c63830b8d1d5e
Utsavi009 <utsavikathiriya@gmail.com>
2021-05-27 16:42:23 +0200

    completes till second task

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import '../App.css';
 
 
 const PokemonList = ({data}) => {
@@ -20,7 +19,6 @@ const PokemonList = ({data}) => {
                   type="text"
                   placeholder="Search.."
                   onChange={(event) => setSearch(event.target.value)}
-                  autofocus
                 />
                 {data &&
                   data
